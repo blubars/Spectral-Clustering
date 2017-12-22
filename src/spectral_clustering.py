9e61167d1a7c116728cb2df3f0d0cbbaf360a303
blubars <blubars@gmail.com>
2017-12-22 15:41:16 -0700

    added profiling code and results

--- src/spectral_clustering.py
+++ src/spectral_clustering.py
@@ -14,17 +14,24 @@ import numpy as np
 from numpy.linalg import norm
 from sklearn import datasets
 from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 import scipy.linalg as sla
 import timeit
+import random
+
+SUBPLOT_COLS = 4
+SEED = 212
+random.seed(SEED)
 
 plot_colors = ['#377eb8', '#ff7f00', '#4daf4a']
 
 ###############################################################
 #  UTILITY FUNCTIONS
 ###############################################################
-# create toy datasets for cluster experiments: from sklearn
+# create toy datasets for cluster experiments: 
+# this function was modified from sklearn.
 def generate_datasets(num_samples=500):
     noisy_circles = datasets.make_circles(
             n_samples=num_samples, factor=0.5, noise=0.05)
@@ -51,21 +58,21 @@ def plot_results(X, y, fignum, title):
     plt.gca().set_aspect('equal')
     plt.show()
 
-def add_subplot(subplot_list, X, y, title, plot_type=None, limits=None):
+def add_subplot(subplot_list, X, y, title, plot_type=None, limits=None, accuracy=None):
     # plot types: None, 'sc-circle'
     subplot_list.append((X, y, title, plot_type, limits))
 
 def plot_subplots(fignum, subplot_list):
     # make subplots and display
     num_plots = len(subplot_list)
-    num_rows = num_plots // 3
-    if num_plots % 3 != 0:
+    num_rows = num_plots // SUBPLOT_COLS
+    if num_plots % SUBPLOT_COLS != 0:
         num_rows += 1
-    if num_plots < 3:
+    if num_plots < SUBPLOT_COLS:
         num_cols = num_plots
     else:
-        num_cols = 3
-    plot_shape = (2*num_cols+2,2*num_rows+2)
+        num_cols = SUBPLOT_COLS
+    plot_shape = (2*num_cols+2,1.8*num_rows+2)
     fig = plt.figure(fignum, plot_shape)
     for i, subplot_item in enumerate(subplot_list):
         #print("rows:{}, cols:{}, i:{}".format(num_rows, num_cols, i+1))
@@ -85,6 +92,112 @@ def plot_subplots(fignum, subplot_list):
         ax.set_title(title, fontsize='10')
     fig.tight_layout()
     plt.show()
+
+def calc_class_accuracy(Y, Y_pred, label1, label2):
+    #print("finding acc btw {} and {}".format(label1, label2))
+    correct = 0
+    total = 0
+    filt = np.where(Y == label1)
+    preds = Y_pred[filt]
+    for p in preds:
+        if p == label2:
+            correct += 1
+        total += 1
+    acc = correct / total
+    #print("Accuracy ({},{}): {}".format(label1, label2, acc))
+    return acc
+
+def calc_align_accuracy(Y, Y_pred):
+    # automatically find label alignments and accuracy:
+    # try every alignment of label :(
+    labels = np.unique(Y)
+    aligns = []
+    accuracies = []
+    for i,l1 in enumerate(labels):
+        aligns.append([])
+        for l2 in labels:
+            aligns[i].append((l1,l2))
+    for i, row in enumerate(aligns):
+        if i == len(aligns) - 1:
+            # don't check the last row, should be only one
+            break
+        found = False
+        for j, algn in enumerate(row):
+            if found:
+                aligns[i][j] = None
+            elif algn:
+                acc = calc_class_accuracy(Y, Y_pred, algn[0], algn[1])
+                if acc < 0.5:
+                    # bad alignment, hopefully. or just awful accuracy
+                    aligns[i][j] = None
+                else:
+                    # found it! remove possibility from other rows
+                    found = True
+                    foundlabel = algn[1]
+                    for k,l3 in enumerate(labels):
+                        if l3 == algn[0]:
+                            continue
+                        else:
+                            aligns[k][foundlabel] = None
+                    # now set rest of possibilities here to false
+    # return list of alignments
+    final_aligns = []
+    align_dict = {}
+    for row in aligns:
+        for a in row:
+            if a is not None:
+                align_dict[a[1]] = a[0]
+                final_aligns.append(a)
+    # get overall acc w/ dict mapping
+    return calc_accuracy(Y, Y_pred, align_dict)
+
+def calc_accuracy(Y, Y_pred, mapping=None):
+    if mapping:
+        mapped = np.copy(Y_pred)
+        for i,label in enumerate(mapped):
+            mapped[i] = mapping[label]
+    else:
+        mapped = Y_pred
+    correct = 0
+    # try combos of labels
+    for y, y_pred in zip(Y, mapped):
+        if y == y_pred:
+            correct += 1
+    return correct / len(Y)
+
+def cluster_and_plot_dataset(X, y, n_clusters, plots, sigma_sq=None):
+    # run spectral clustering, KMeans, and plot
+    print("Running spectral clustering, original")
+    model = SpectralClustering(num_clusters=n_clusters, sigma_sq=sigma_sq)
+    y_pred = model.fit_predict(X)
+    acc_sc = calc_align_accuracy(y, y_pred)
+    print("SC Accuracy: {}".format(acc_sc))
+    km = KMeans(n_clusters=n_clusters, n_init=10)
+    km.fit(X)
+    acc_km = calc_align_accuracy(y, km.labels_)
+    print("KM Accuracy: {}".format(acc_km))
+    add_subplot(plots, X, y, "Ground Truth")
+    add_subplot(plots, X, km.labels_, "KMeans")
+    add_subplot(plots, model._LX, y_pred, 
+                "Spectral clustering:\nembedded domain",
+                plot_type='sc-circle')
+    add_subplot(plots, X, y_pred, 
+                "Spectral clustering:\noriginal domain")
+
+# alternate main to run profiling of spectral clustering
+def run_profiling(fast=False, percent=0.2):
+    n_samps = [100, 500, 1000, 2000]
+    res = []
+    for n in n_samps:
+        print("Profiling spectral clustering, dataset size={}".format(n))
+        datasets = generate_datasets(n)
+        X, y = datasets[0]
+        model = SpectralClustering(num_clusters=2, sigma_sq=0.01)
+        #y_pred = model.fit_predict(X)
+        res.append(model.profile(X, knn=fast, y=y, percent=percent))
+    print("(n_samples, time)")
+    for n, t in zip(n_samps, res):
+        print("({}, {})".format(n, t))
 
 ###############################################################
 #  CLASS: SPECTRAL CLUSTERING:
@@ -125,12 +238,33 @@ class SpectralClustering:
         LX = self.make_eigenvector_matrix(L, Dinvsq)
         # (4) -------
         # Finally, do clustering on reduced space using KMeans:
-        km = KMeans(n_clusters=self.num_clusters, n_init=20)
-        km.fit(LX)
-        self._labels = km.labels_
+        self._labels = self.partition_embedding(LX)
         self.auto_eval_cluster()
         return self._labels
 
+    def fit_predict_fast(self, X, n_neighbors=3, ratio=0.2):
+        # try to speed up clustering by only running
+        # spectral clustering on part of the dataset, then
+        # using knn for the rest
+        splitx = round(ratio*len(X))
+        Xs = X[0:splitx]
+        Xk = X[splitx:]
+        ys = self.fit_predict(Xs)
+        yk = self.knn_predict(Xs, ys, Xk)
+        return np.append(ys, yk)
+
+    # try to classify other points using KNN
+    def knn_predict(self, X, y, X_pred, n_neighbors=3):
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+        knn.fit(X, y)
+        return knn.predict(X_pred)
+
+    def partition_embedding(self, LX):
+        # do clustering on reduced space using KMeans:
+        km = KMeans(n_clusters=self.num_clusters, n_init=10)
+        km.fit(LX)
+        labels = km.labels_
+        return labels
 
     def auto_eval_cluster(self):
         # 'cluster quality' can be approximated by tightness of
@@ -223,46 +357,49 @@ class SpectralClustering:
             #                 eigvals[1]*eigvects[:,1]))
         return LX
 
+    def profile(self, X, y=None, repeat=8, knn=False, percent=0.2):
+        t = timeit.default_timer
+        times = []
+        # profile each step, returning fastest run from each step
+        print("Profiling({}x)... ".format(repeat), end=' ')
+        for i in range(repeat):
+            print("{}".format(i), end=' ', flush=True)
+            if knn:
+                t_start = t()
+                yp = self.fit_predict_fast(X, ratio=percent)
+                t_end = t()
+            else:
+                t_start = t()
+                yp = self.fit_predict(X)
+                t_end = t()
+            times.append(t_end-t_start)
+            # evaluate accuracy
+            if y is not None:
+                acc = calc_align_accuracy(y, yp)
+                #print("Accuracy: {}".format(acc))
+                if (acc < 0.6):
+                    print("x", end=' ')
+                    if i == 0:
+                        plot_results(X, yp, 1, "")
+        times.sort()
+        print("\nBest time: {}".format(times[0]))
+        return times[0]
 
 ###############################################################
 #  MAIN: Test on toy datasets and plot
 ###############################################################
 def main():
-    t = timeit.default_timer
-    datasets = generate_datasets()
+    datasets = generate_datasets(200)
     plots = []
-
     X, y = datasets[0]
-    print("Running spectral clustering, original")
-    model = SpectralClustering(num_clusters=2, sigma_sq=0.02)
-    t_start = t()
-    y_pred = model.fit_predict(X)
-    t_end = t()
-    print("Time: {}".format(t_end-t_start))
-
-    add_subplot(plots, X, y, "Ground Truth")
-    add_subplot(plots, model._LX, y_pred, 
-                "Spectral clustering:\nembedded domain",
-                plot_type='sc-circle')
-    add_subplot(plots, X, y_pred, 
-                "Spectral clustering:\noriginal domain")
-
+    cluster_and_plot_dataset(X, y, 2, plots, 0.015)
+    X, y = datasets[1]
+    cluster_and_plot_dataset(X, y, 2, plots, 0.02)
     X, y = datasets[3]
-    print("Running spectral clustering, dataset 3")
-    model = SpectralClustering(num_clusters=3)
-    t_start = t()
-    y_pred = model.fit_predict(X)
-    t_end = t()
-    print("Time: {}".format(t_end-t_start))
-
-    add_subplot(plots, X, y, "Ground Truth")
-    add_subplot(plots, model._LX, y_pred, 
-                "Spectral clustering:\nembedded domain",
-                plot_type='sc-circle')
-    add_subplot(plots, X, y_pred, 
-                "Spectral clustering:\noriginal domain")
+    cluster_and_plot_dataset(X, y, 3, plots)
     plot_subplots(1, plots)
 
 if __name__ == "__main__":
+    #run_profiling(True, percent=0.75)
     main()
 
